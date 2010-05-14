c5a5dca212756eda1b14bb3b031981f928f8f173
Doc Yes <spam@featureblend.com>
2010-05-14 10:38:12 -0700

    Add jsdocs back. Migrate to rex in string parser.

--- lilswf.js
+++ lilswf.js
@@ -1,3 +1,7 @@
+/**
+ * lil lib to help you detect flash.
+ * NOTE: I am not production ready yet.
+ */
 var lilswf = function(){
     var window,
         undefined,
@@ -5,6 +9,14 @@ var lilswf = function(){
         self.raw = "",
         self.version = [],
         self.installed = self.isCool = false;
+    /**
+     * A safe accessor for the native ActiveX GetVariable method.
+     * 
+     * @param {Object} activeXObj A reference to an ActiveXObject.
+     * @param {String} name The variable name for lookup.
+     * @type {String}
+     * @return The value of the AxtiveX if it is found or an empty string.
+     */       
     function activeXObjectGetVariable(activeXObj, name){
         try{
             return activeXObj.GetVariable(name);
@@ -12,6 +24,13 @@ var lilswf = function(){
             return "";
         }
     }
+    /**
+     * A safe accessor for creating a native ActiveX object 
+     * 
+     * @param {String} name The ActiveX object name lookup.
+     * @type Object || undefined
+     * @return One of an ActiveX object or undefined.
+     */    
     function newActiveObject(name){
         try{
             return new ActiveXObject(name);
@@ -19,16 +38,28 @@ var lilswf = function(){
             return undefined;
         }
     }
+    /**
+     * Parse an ActiveX $version variable into properly casted members:
+     * 
+     * @param {String} raw The ActiveX Object GetVariable($version) string return value. 
+     * @type Array
+     * @return An array of integers casted from the original raw version string. Values that can't be casted are returned as a -1 value.
+     */    
     function parseActiveXVersionVariable(raw){
-        var parts = raw.split(","),
-            version = [],
+        var parts = raw.match(/[0-9]+/g),
             i = 0,
             l = parts.length;
         for(; i < l; i++){
-            version[i] = parseInt(parts[i], 10) || -1;
+            parts[i] = parts[i], 10);
         }
-        return version;
+        return parts;
     }
+    /**
+     * TBD.
+     * 
+     * @params arguments
+     * @type Boolean
+     */    
     self.atLeast = function(){
         return false;
     };
