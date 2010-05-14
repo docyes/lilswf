ee7cbd29ca41c37aab2a4bf15001b606188a0d08
Doc Yes <spam@featureblend.com>
2010-05-13 17:09:36 -0700

    Cleanup all ActiveX methods and signatures.

--- lilswf.js
+++ lilswf.js
@@ -1,109 +1,66 @@
+/**
+ * lil lib to help you detect flash.
+ * NOTE: I am not done and production ready yet.
+ */
 var lilswf = function(){
-    var self = this,
-        /**
-         * Object literal syntax for expressing activeX detection.
-         * {String} n
-         * {Function} v A function to call to validate the activeX object check against.
-         */
-        axRules = [
-           {
-              "n":"ShockwaveFlash.ShockwaveFlash.7",
-              "v":function(obj){
-                  return axV(obj);
-              }
-           },
-           {
-              "n":"ShockwaveFlash.ShockwaveFlash.6",
-              "v":function(obj){
-              var version = "6,0,21";
-              try{
-                  obj.AllowScriptAccess = "always";
-                      version = axV(obj);
-                  }catch(err){}
-                  return version;
-              }
-           },
-           {
-              "n":"ShockwaveFlash.ShockwaveFlash",
-              "v":function(obj){
-                  return axV(obj);
-              }
-           }
-        ];
-        self.v = genV();
-    function genV(){
-        return (navigator.appVersion.indexOf("Mac")==-1 && window.execScript)?axRulesParser():{installed: false, raw:"", major:-1, minor:-1, revision:-1, revisionStr:""}
-    }
+    var self = this;
+    var undefined;
     /**
-     * TBD.
-     */
-    function axRulesParser(){
-        var info = {installed: false, raw:"", major:-1, minor:-1, revision:-1, revisionStr:""};
-        if(navigator.appVersion.indexOf("Mac")==-1 && window.execScript){
-            var version = -1;
-            for(var i=0; i<axRules.length && version==-1; i++){
-                var axRule = axRules[i];
-                var obj = axObj(axRule.name);
-                if(!obj.activeXError){
-                    info.installed = true;
-                    version = axRule.version(obj);
-                    if(version!=-1){
-                        var v = axParser(version);
-                        info.raw = v.raw;
-                        info.major = v.major;
-                        info.minor = v.minor;
-                        info.revision = v.revision;
-                        info.revisionStr = v.revisionStr;
-                    }
-                }
-            }
-        }
-        return info;
-    }
-    /**
-     * Gracefully extract the ActiveX version of the plugin.
+     * A safe accessor for the native ActiveX GetVariable method.
+     * 
+     * @param {Object} activeXObj A reference to an ActiveXObject.
+     * @param {String} name The variable name for lookup.
+     * @type {String}
+     * @return The value of the AxtiveX if it is found or an empty string.
+     * 
+     * 
+     * Returns the value of the Flash variable specified by name. Returns null if the variable does not exist. The argument type is string.
      * 
      * @param {Object} activeXObj The flash ActiveX object.
      * @type String
      */
-    function axV(axObj){
-        var v = -1;
+    function activeXObjectGetVariable(activeXObj, name){
+        var value = "";
         try{
-            v = axObj.GetVariable("$version");
-        }catch(err){}
-        return v;
+            value = activeXObj.GetVariable(name);
+        }catch(e){}
+        return value;
     }
     /**
-     * Try and retrieve an ActiveX object having a specified name.
+     * A safe accessor for creating a native ActiveX object 
      * 
      * @param {String} name The ActiveX object name lookup.
-     * @return One of ActiveX object or a simple object having an attribute of activeXError with a value of true.
-     * @type Object
+     * @type Object || undefined
+     * @return One of an ActiveX object or undefined.
      */
-    function axObj(name){
-        var obj = -1;
+    function newActiveObject(name){
+        var obj = undefined;
         try{
             obj = new ActiveXObject(name);
-        }catch(err){
-            obj = {activeXError:true};
-        }
+        }catch(e){}
         return obj;
     }
     /**
-     * Parse an ActiveX $version string into an object.
+     * Parse an ActiveX $version variable into properly casted members:
      * 
      * @param {String} str The ActiveX Object GetVariable($version) return value. 
      * @return An object having raw, major, minor, revision and revisionStr attributes.
      * @type Object
+     * @return An object literal having the following properties:
+     * {String} raw
+     * {Number} major 
+     * {Number} minor
+     * {Number} revision 
+     * {String} revisionString
      */
-    function axVParser(str){
+    function parseActiveXVersionVariable(str){
         var v = str.split(",");
         return {
-            "raw":str,
-            "major":parseInt(v[0].split(" ")[1], 10),
-            "minor":parseInt(v[1], 10),
-            "revision":parseInt(v[2], 10),
-            "revisionStr":v[2]
+            raw: str,
+            major: parseInt(v[0].split(" ")[1], 10),
+            minor: parseInt(v[1], 10),
+            revision: parseInt(v[2], 10),
+            revisionString: v[2]
         };
     }
     return self;
